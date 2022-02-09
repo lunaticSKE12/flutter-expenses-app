2607eebe2d2ae601f1f18962c87d84eda3762876
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-09 19:44:04 +0700

    Styling a Container

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,11 @@ class MyHomePage extends StatelessWidget {
                 return Card(
                   child: Row(children: <Widget>[
                     Container(
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black, width: 2)),
+                      padding: EdgeInsets.all(10),
                       child: Text(tx.amount.toString()),
                     ),
                     Column(
