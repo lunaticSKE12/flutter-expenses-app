ad7ffc41ba8c79817169c22282755a9573aae3ba
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-10 20:32:25 +0700

    Splitting the App Into Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
-import './tansaction.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,21 +16,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: 't1',
-      title: 'New Shoes',
-      amount: 69.99,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: 't2',
-      title: 'Weekly Groceries',
-      amount: 16.53,
-      date: DateTime.now(),
-    ),
-  ];
-
   // late String titleInput;
   // late String amountInput;
   final titleController = TextEditingController();
@@ -56,74 +39,6 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              elevation: 5,
-              child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: <Widget>[
-                        TextField(
-                          decoration: InputDecoration(labelText: 'Title'),
-                          controller: titleController,
-                          // onChanged: (value) {
-                          //   titleInput = value;
-                          // },
-                        ),
-                        TextField(
-                          decoration: InputDecoration(labelText: 'Amount'),
-                          controller: amountController,
-                          // onChanged: (value) {
-                          //   amountInput = value;
-                          // },
-                        ),
-                        FlatButton(
-                          onPressed: () {
-                            // print(titleInput);
-                            // print(amountInput);
-                            print(titleController.text);
-                          },
-                          child: Text('Add Transaction'),
-                          textColor: Colors.purple,
-                        )
-                      ])),
-            ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(children: <Widget>[
-                    Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        '\$${tx.amount}',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.purple),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          tx.title,
-                          style: TextStyle(
-                              fontSize: 18, fontWeight: FontWeight.bold),
-                        ),
-                        Text(
-                          DateFormat.yMMMd().format(tx.date),
-                          style: TextStyle(color: Colors.grey),
-                        )
-                      ],
-                    )
-                  ]),
-                );
-              }).toList(),
-            )
           ],
         ));
   }
