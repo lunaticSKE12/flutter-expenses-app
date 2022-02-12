ccb7d14795b14ca757012172c922c8f1844ab1d5
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2022-02-12 14:32:44 +0700

    Making the List Scrollable!

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,11 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter App'),
-        ),
-        body: Column(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
@@ -42,6 +43,8 @@ class MyHomePage extends StatelessWidget {
             ),
             UserTransactions()
           ],
-        ));
+        ),
+      ),
+    );
   }
 }
